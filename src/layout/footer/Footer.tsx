7db9800be2745ff59b748a3454d3854cad29d523
VitaMirosh <miroshnichenko.vitaliya1992@gmail.com>
2025-11-17 19:48:08 +0300

    decorate and fix section footer

--- src/layout/footer/Footer.tsx
+++ src/layout/footer/Footer.tsx
@@ -1,6 +1,7 @@
 import styled from "styled-components";
 import {Icon} from "../../components/icon/Icon.tsx";
 import {FlexWrapper} from "../../components/FlexWrapper.tsx";
+import {theme} from "../../styles/Theme.ts";
 
 
 export const Footer = () => {
@@ -11,7 +12,7 @@ export const Footer = () => {
         <SocialList>
           <SocialItem>
             <SocialLink>
-              <Icon height={"21px"} width={"21px"} viewBox={"0 0 21px 21px"} iconId={"instagram"}/>
+              <Icon height={"21px"} width={"21px"}  viewBox={"0 0 21px 21px"} iconId={"instagram"}/>
             </SocialLink>
           </SocialItem>
 
@@ -23,7 +24,7 @@ export const Footer = () => {
 
           <SocialItem>
             <SocialLink>
-              <Icon height={"21px"} width={"21px"} viewBox={"0 0 21px 21px"} iconId={"link"}/>
+              <Icon height={"21px"} width={"21px"}  viewBox={"0 0 21px 21px"} iconId={"link"}/>
             </SocialLink>
           </SocialItem>
 
@@ -36,16 +37,21 @@ export const Footer = () => {
 };
 
 const StyledFooter = styled.footer`
-    background-color: #ffc267;
-    min-height: 30vh;
+    background-color: ${theme.colors.secondaryBg};
+    padding:40px 0
 
 `
 const Name = styled.span`
+    font-family: "Josefin Sans, sans-serif";
+    font-weight: 700;
+    font-size: 22px;
+    letter-spacing: 3px;
 
 `
 const SocialList = styled.ul`
     display: flex;
-    gap: 30px;
+    gap: 20px;
+    margin: 30px 0;
 
 `
 const SocialItem = styled.li`
@@ -53,9 +59,28 @@ const SocialItem = styled.li`
 `
 
 const SocialLink = styled.a`
+    background-color: rgba(255, 255, 255, 0.69);
+    border-radius: 50px;
+    width: 35px;
+    height: 35px;
 
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    
+     color:${theme.colors.accent} ;
+    &:hover {
+        color:${theme.colors.font};
+        transform:translateY(-4px);
+    }
 `
 
 const Copyright = styled.small`
+    font-weight: 400;
+    font-size: 12px;
+    text-align: center;
+    color: rgba(255, 255, 255, 0.5);
+
+
 
 `
