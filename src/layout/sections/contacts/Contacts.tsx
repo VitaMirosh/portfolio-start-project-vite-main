fdd589377aa717d5a246803503119d8882d62d5d
VitaMirosh <miroshnichenko.vitaliya1992@gmail.com>
2025-11-17 19:46:21 +0300

    decorate and fix section contacts

--- src/layout/sections/contacts/Contacts.tsx
+++ src/layout/sections/contacts/Contacts.tsx
@@ -1,35 +1,65 @@
 import styled from "styled-components";
 import {SectionTitle} from "../../../components/SectionTitle.tsx";
 import {Button} from "../../../components/Button.tsx";
+import {Container} from "../../../components/Container.ts";
+import {theme} from "../../../styles/Theme.ts";
 
 export const Contact = () => {
   return (
     <StyledContact>
-      <SectionTitle>Contact</SectionTitle>
-      <StyledForm>
-        <Field placeholder={"name"}/>
-        <Field placeholder={"subject"}/>
-        <Field placeholder={"message"} as={"textarea"}/>
-        <Button type={"submit"}>Send message</Button>
-      </StyledForm>
+      <Container>
+        <SectionTitle>Contact</SectionTitle>
+        <StyledForm>
+          <Field placeholder={"name"}/>
+          <Field placeholder={"subject"}/>
+          <Field placeholder={"message"} as={"textarea"}/>
+          <Button type={"submit"}>Send message</Button>
+        </StyledForm>
+      </Container>
+
     </StyledContact>
   );
 };
 
 const StyledContact = styled.section`
-    min-height: 50vh;
-    background-color: #fbffd8;
+
 `
 const StyledForm = styled.form`
-    max-width: 500px;
+    max-width: 540px;
     width: 100%;
     display: flex;
     flex-direction: column;
-    gap: 10px;
+    align-items: center;
+    gap: 16px;
     margin: 0 auto;
+    
+   & textarea{
+        resize: none;
+        height: 155px;
+    }
+    
 
 `
 const Field = styled.input`
+    width: 100%;
+    background-color:${theme.colors.highlight} ;
+    border:none;
+    padding: 7px 15px;
+
+    font-family: "Poppins", sans-serif;
+    font-weight: 400;
+    font-size: 12px;
+    letter-spacing: 0.05em;
+    color: ${theme.colors.font};
+    
+    &::placeholder {
+        color: ${theme.colors.placeholderColor};
+        text-transform: capitalize;
+       
+    }
+ &:focus-visible{
+     outline:1px solid ${theme.colors.accent};
+ }  
 
 `
 
