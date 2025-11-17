bf80a39e46e858cc3c7c12b8f5018347ecd1984d
VitaMirosh <miroshnichenko.vitaliya1992@gmail.com>
2025-11-17 19:47:21 +0300

    decorate and fix section works

--- src/layout/sections/works/Works.tsx
+++ src/layout/sections/works/Works.tsx
@@ -1,10 +1,11 @@
 import styled from "styled-components";
 import {SectionTitle} from "../../../components/SectionTitle.tsx";
-import {Menu} from "../../../components/menu/Menu.tsx";
+import {TabMenu} from "./tabMenu/TabMenu.tsx";
 import {FlexWrapper} from "../../../components/FlexWrapper.tsx";
 import {Work} from "./work/Work.tsx"
 import socialImg from "../../../assets/images/pic11.webp";
 import socialImg2 from "../../../assets/images/pic22.webp"
+import {Container} from "../../../components/Container.ts";
 
 
 const workItems = ["All", "landing page", "React", "spa"]
@@ -12,21 +13,22 @@ const workItems = ["All", "landing page", "React", "spa"]
 export const Works = () => {
   return (
     <StyledWorks>
-      <SectionTitle>My works</SectionTitle>
-      <Menu menuItems={workItems}/>
-      <FlexWrapper justify={"space-around"}>
-        <Work title={"Social Network"}
-              text={"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim. Lorem ipsum dolor sit amet, consectetur adipisicing elit."}
-              src={socialImg}/>
-        <Work title={"Timer"}
-              text={"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim. Lorem ipsum dolor sit amet, consectetur adipisicing elit  ut labore et dolore magna aliqua Ut enim"}
-              src={socialImg2}/>
-      </FlexWrapper>
+      <Container>
+        <SectionTitle>My works</SectionTitle>
+        <TabMenu menuItems={workItems}/>
+        <FlexWrapper justify={"space-between"} aling={"flex-start"}>
+          <Work title={"Social Network"}
+                text={"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim. Lorem ipsum dolor sit amet, consectetur adipisicing elit."}
+                src={socialImg}/>
+          <Work title={"Timer"}
+                text={"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim. Lorem ipsum dolor sit amet, consectetur adipisicing elit  ut labore et dolore magna aliqua Ut enim"}
+                src={socialImg2}/>
+        </FlexWrapper>
+      </Container>
     </StyledWorks>
   );
 };
 
 const StyledWorks = styled.section`
-    min-height: 100vh;
-    background-color: #94bcff;
+   
 `
