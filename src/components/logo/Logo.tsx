e5bd2ccabeb0d73a3077216386762e3a259ce36a
VitaMirosh <miroshnichenko.vitaliya1992@gmail.com>
2025-11-25 21:36:45 +0300

    TypeWriter-effect in main

--- src/components/logo/Logo.tsx
+++ src/components/logo/Logo.tsx
@@ -1,6 +1,7 @@
 import {Icon} from "../icon/Icon.tsx";
 
 
+
 export const Logo = () => {
   return (
     <a href="">
--- src/layout/sections/main/Main.tsx
+++ src/layout/sections/main/Main.tsx
@@ -3,6 +3,8 @@ import {FlexWrapper} from "../../../components/FlexWrapper.ts";
 import {Container} from "../../../components/Container.ts";
 import * as React from "react";
 import {S} from './Main_Styles.ts';
+import Typewriter from 'typewriter-effect';
+
 
 
 export const Main: React.FC = () => {
@@ -13,7 +15,18 @@ export const Main: React.FC = () => {
           <div>
             <S.SmallText>Hi There</S.SmallText>
             <S.Name>I am <span>Vitaliia Miroshnychenko</span></S.Name>
-            <S.MainTitle>A Frontend Developer.</S.MainTitle>
+            {/*<S.MainTitle>A Frontend Developer.</S.MainTitle>*/}
+            <S.MainTitle>
+              <p>A Frontend Developer.</p>
+              <Typewriter
+                options={{
+                  strings: ["A Frontend Developer."],
+                  autoStart: true,
+                  loop: true,
+                  delay:150
+                }}
+              />
+            </S.MainTitle>
           </div>
           <S.PhotoWrapper>
             <S.Photo src={photo} alt=""/>
